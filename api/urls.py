83e05f3bf1dbf3c5f4a2f792014c3e19a9eb4f4f
Vaibhav Ingle <vingle@unomaha.edu>
2017-09-12 15:45:36 -0500

    added endpoints for events and IFTTT

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,7 @@ from django.views.decorators.csrf import csrf_exempt
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+
     url(r'^session', csrf_exempt(controllers.Session.as_view())),
     url(r'^register', csrf_exempt(controllers.Register.as_view())),
     url(r'^events', csrf_exempt(controllers.Events.as_view())),
--- django_backend/settings.py
+++ django_backend/settings.py
@@ -27,7 +27,7 @@ if ENVIRONMENT == 'PROD':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['137.48.185.230', 'localhost']
+ALLOWED_HOSTS = ['24.252.6.207', 'localhost','192.168.99.100','192.168.0.4','192.168.0.1']
 
 
 # Application definition
